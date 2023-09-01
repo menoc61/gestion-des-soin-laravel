2cb3d9e9c1399d15fe48cafa988123d38f78f1e0
menoc61 <gillemomeni@gmail.com>
2023-09-01 10:42:51 +0200

    send test to database

--- database/migrations/2020_09_16_230135_create_tests_table.php
+++ database/migrations/2020_09_16_230135_create_tests_table.php
@@ -16,8 +16,43 @@ class CreateTestsTable extends Migration
         Schema::create('tests', function (Blueprint $table) {
             $table->id();
             $table->string('test_name');
+            $table->string('diagnostic_type');
             $table->mediumText('comment')->nullable();
-            $table->string('signes_particuliers_peau');
+
+            $table->string('signes_particuliers_peau')->nullable();
+
+            $table->string('Etat_generale_des_mains')->nullable();
+            $table->string('Etat_des_ongles_mains')->nullable();
+            $table->string('signes_particuliers_mains')->nullable();
+            $table->string('signes_particuliers_ongles_mains')->nullable();
+            $table->string('soinList_main')->nullable();
+            $table->string('vernisInput_main')->nullable();
+            $table->string('obserationInput_main')->nullable();
+            $table->string('reliefInput_main')->nullable();
+            $table->string('cicatrices_main')->nullable();
+            $table->string('callosites_main')->nullable();
+            $table->string('spInput_main')->nullable();
+            $table->string('skinStateInput_main')->nullable();
+            $table->string('tache_main')->nullable();
+            $table->string('cicatrices_main_dorsal')->nullable();
+            $table->string('callosite_main_dorsal')->nullable();
+            $table->string('spInput_main_dorsal')->nullable();
+
+            $table->string('Etat_generale_des_pieds')->nullable();
+            $table->string('Etat_des_ongles_pieds')->nullable();
+            $table->string('signes_particuliers_pieds')->nullable();
+            $table->string('signes_particuliers_ongles_pieds')->nullable();
+            $table->string('soinList_pied')->nullable();
+            $table->string('vernisInput_pied')->nullable();
+            $table->string('obserationInput_pied')->nullable();
+            $table->string('etat_pieds')->nullable();
+            $table->string('taches_pieds')->nullable();
+            $table->string('aureoles_pieds')->nullable();
+            $table->string('veines_face_ext_pieds')->nullable();
+            $table->string('veines_face_int_pieds')->nullable();
+            $table->string('douleur_talon_pieds')->nullable();
+            $table->string('spInput_pieds')->nullable();
+
             $table->timestamps();
         });
     }
