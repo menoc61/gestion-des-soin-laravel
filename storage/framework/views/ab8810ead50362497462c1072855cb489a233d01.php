bbb79e865963e611aac197e2c36fea01ee07a13a
menoc61 <gillemomeni@gmail.com>
2023-10-20 10:37:18 +0100

    fix conflit

--- storage/framework/views/ab8810ead50362497462c1072855cb489a233d01.php
+++ storage/framework/views/ab8810ead50362497462c1072855cb489a233d01.php
@@ -5,6 +5,9 @@
 
 <?php $__env->startSection('content'); ?>
 
+<div class="mb-3">
+    <button class="btn btn-primary" onclick="history.back()">Retour</button>
+</div>
 
 <div class="card shadow mb-4">
    <div class="card-header py-3">
