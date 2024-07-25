5918f2e9c23e365eebbf794bff47fb339edbf1ff
fresnel25 <ngaleufresnel@gmail.com>
2024-07-25 15:27:31 +0100

    synchronisation login

---
+++ resources/views/globalHome.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+    <title>ERP- saï_i_lama</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('dashboard/css/sb-admin-2.min.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="js-preloader" class="js-preloader">
+        <div class="preloader-inner">
+            <span class="dot"></span>
+            <div class="dots">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Preloader End ***** -->
+
+
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Main Banner Area Start ***** -->
+    <section id="section-1">
+        <div class="content-slider">
+            <input type="radio" id="banner1" class="sec-1-input" name="banner" checked>
+
+            <div class="slider">
+                <div id="top-banner-1" class="banner">
+                    <div class="banner-inner-wrapper header-text">
+                        <h1 class="text-secondary"
+                            style="color: linear-gradient(-45deg, rgb(238,130,238), rgb(139,0,139));">BIENVENUE SUR ERP
+                            saï i lama</h1>
+                        <!-- <div class="main-caption">
+              <h2>Take a Glimpse Into The Beautiful Country Of:</h2>
+              <h1>Saï i lama</h1>
+              <div class="border-button"><a href="#">Se connecter </a></div>
+            </div> -->
+                        <div class="col-md-12 col-sm-12 col-12">
+                            <center>
+                                <div class="d-flex w-25 justify-content-center align-items-center">
+                                    <img src="{{ asset('img/sai-i-lama-logo.png') }}" class="img-fluid">
+                                </div>
+                            </center>
+                        </div>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="more-info">
+                                        <div class="row">
+                                            <div class="col-lg-4 col-sm-6 col-12">
+                                                <a href="INDE.html" class="app-icon"
+                                                    style="border: 1px solid #079939; margin-bottom:16px;">
+                                                    <img src="{{ asset('img/icons8-person-96.png') }}"
+                                                        alt="academy pro">
+                                                    <h4>Module Ressources Humaines</h4>
+                                                </a>
+                                            </div>
+                                            <div class="col-lg-4 col-sm-6 col-12">
+                                                <a href="http://127.0.0.1:3001/dashboard" class="app-icon"
+                                                    style="border: 1px solid #6A037A; margin-bottom:16px;">
+                                                    <img src="{{ asset('img/icons8-ventes-totales-80.png') }}"
+                                                        alt="academy pro">
+                                                    <h4>Module gestion des Stocks</h4>
+                                                </a>
+                                            </div>
+                                            <div class="col-lg-4 col-sm-6 col-12">
+                                                <a href="http://localhost:8000/home" class="app-icon"
+                                                    style="border: 1px solid #079939;">
+                                                    <img src="{{ asset('img/icons8-millénaire-100.png') }}"
+                                                        alt="academy pro">
+                                                    <h4>Module Soins</h4>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+    </section>
+
+
+</body>
+
+</html>
